a44436e15b465885f5a6972d6e911a1b6f00eaa9
elianddb <elianddb@users.noreply.github.com>
2026-05-06 20:47:40 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/libraries/doltcore/sqle/dprocedures/dolt_rebase.go
+++ go/libraries/doltcore/sqle/dprocedures/dolt_rebase.go
@@ -30,11 +30,11 @@ import (
 	"github.com/dolthub/dolt/go/libraries/doltcore/env/actions"
 	"github.com/dolthub/dolt/go/libraries/doltcore/merge"
 	"github.com/dolthub/dolt/go/libraries/doltcore/rebase"
-	"github.com/dolthub/dolt/go/store/datas"
 	"github.com/dolthub/dolt/go/libraries/doltcore/ref"
 	"github.com/dolthub/dolt/go/libraries/doltcore/sqle/dfunctions"
 	"github.com/dolthub/dolt/go/libraries/doltcore/sqle/dsess"
 	"github.com/dolthub/dolt/go/libraries/utils/argparser"
+	"github.com/dolthub/dolt/go/store/datas"
 )
 
 var doltRebaseProcedureSchema = []*sql.Column{
